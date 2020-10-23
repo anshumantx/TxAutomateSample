bffcd8f325526154c5626cf05d6dde8347221db7
anshuman.singh@testingxperts.com <anshuman.singh@testingxperts.com>
2020-10-23 18:42:17 +0530

    Initial Commit

---
+++ TxAutomateTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TxAutomateTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
